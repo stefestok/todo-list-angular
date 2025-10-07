3c0ac134b405c4dc2f4df4e26261d1c7a8b23abe
Steve <stefestok@gmail.com>
2025-10-07 16:02:14 +0200

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TodoList } from "./components/todo-list/todo-list/todo-list";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, TodoList],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('todo-list-angular');
+}
---
+++ src/app/components/todo-list/todo-list/todo-list.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.html',
+  styleUrl: './todo-list.scss'
+})
+export class TodoList {
+  todos = [
+    { text: 'Precvičenie Angularu', completed: false },
+    { text: 'Vytvoriť todo-list appku', completed: false }
+  ];
+
+  newTodoText = '';
+
+  addTodo() {
+    const text = this.newTodoText.trim();
+    if (text) {
+      this.todos.push({ text, completed: false });
+      this.newTodoText = '';
+    }
+  }
+}
